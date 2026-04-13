43b05b0bf623c28d42c4554739000017810d5235
Nafi-web-dev <nafiislam176@gmail.com>
2026-04-13 21:07:25 +0600

    Add files via upload

---
+++ anime-data.js
@@ -0,0 +1,16 @@
+const animeData = [
+  {
+    name: "The Summer Hikaru Died",
+    img: "the-summer-hikaru-died.jpeg",
+    link: "the-summer-hikaru-died-season.html",
+    tag: "Multi-Audio",
+    ep: "S1-EP12"
+  },
+  {
+    name: "Dr. Stone",
+    img: "dr.stone.jpeg",
+    link: "dr.stoneseason.html",
+    tag: "Multi-Audio",
+    ep: "S4-EP26"
+  }
+];
