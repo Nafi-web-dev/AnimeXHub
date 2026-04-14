cadd8f5b99c2538fe831324c23e839ea45d36c15
Nafi-web-dev <nafiislam176@gmail.com>
2026-04-14 20:52:42 +0600

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -1,5 +1,12 @@
 const animeData = [
   {
+    name: "Release That Witch",
+    img: "release-that-witch.jpeg",
+    link: "release-that-witch-season.html",
+    tag: "Multi-Audio",
+    ep: "S1-EP12"
+  },
+  {
     name: "You and I Are Polar Opposites",
     img: "you-and-i-are-polar-opposites.jpeg",
     link: "you-and-i-are-polar-opposites-season.html",
