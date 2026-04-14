3402cb078601862daa93e3fcb99ed95a3d5c79fc
Nafi-web-dev <nafiislam176@gmail.com>
2026-04-14 20:15:17 +0600

    Add new anime entry 'You and I Are Polar Opposites'

--- anime-data.js
+++ anime-data.js
@@ -1,5 +1,12 @@
 const animeData = [
   {
+    name: "You and I Are Polar Opposites",
+    img: "you-and-i-are-polar-opposites.jpeg",
+    link: "you-and-i-are-polar-opposites-season.html",
+    tag: "Multi-Audio",
+    ep: "S1-EP12"
+  },
+  {
     name: "The Summer Hikaru Died",
     img: "the-summer-hikaru-died.jpeg",
     link: "the-summer-hikaru-died-season.html",
